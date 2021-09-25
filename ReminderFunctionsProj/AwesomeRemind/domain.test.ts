6c0ced90955ec2c68fdb26d296a2d2f4cc9f6c88
satoknj <gk0909c@gmail.com>
2021-09-25 15:13:48 +0900

    add tests

--- ReminderFunctionsProj/AwesomeRemind/domain.test.ts
+++ ReminderFunctionsProj/AwesomeRemind/domain.test.ts
@@ -5,9 +5,30 @@ import * as timezone from 'dayjs/plugin/timezone'
 dayjs.extend(timezone);
 dayjs.tz.setDefault('Asia/Tokyo');
 
-test('isRemindTiming -> true', () => {
-    const now = dayjs('2021-09-22 06:45:00.000');
+describe('remindTiming', () => {;
+    const baseDatetime = dayjs('2021-09-22 06:45:00.000');
     const sut = new RemindTiming([1,2,3,4,5,6,7], 6, 45);
 
-    expect(sut.isRemindTiming(now)).toBe(true);
+    describe.each([
+        { hour: 5, expected: false},
+        { hour: 6, expected: true},
+        { hour: 7, expected: false},
+    ])('hour', ({hour, expected}) => {
+        test(`${expected} with ${hour}`, () => {
+            const now = baseDatetime.set('hour', hour);
+            expect(sut.isRemindTiming(now)).toBe(expected);
+        });
+    });
+
+    describe.each([
+        { minute: 0, expected: false},
+        { minute: 15, expected: false},
+        { minute: 30, expected: false},
+        { minute: 45, expected: true},
+    ])('minute', ({minute, expected}) => {
+        test(`${expected} with ${minute}`, () => {
+            const now = baseDatetime.set('minute', minute);
+            expect(sut.isRemindTiming(now)).toBe(expected);
+        });
+    });
 });
